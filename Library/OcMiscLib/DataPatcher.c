fb770035d4b5c98111cadfbfea2a073273407a7a
jief666 <github.com@jfa.knudsen.ovh>
2020-11-27 15:57:38 +0300

    Improve log.

--- Library/OcMiscLib/DataPatcher.c
+++ Library/OcMiscLib/DataPatcher.c
@@ -16,6 +16,8 @@
 #include <Library/BaseMemoryLib.h>
 #include <Library/DebugLib.h>
 #include <Library/OcMiscLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/PrintLib.h>
 
 #ifdef CLOVER_BUILD
 
@@ -145,22 +147,33 @@ ApplyPatch (
       //
       // Perform replacement.
       //
-      DEBUG((DEBUG_VERBOSE, "Replace " ));
+      UINTN length =
+          (
+               AsciiStrLen("Replace ") +
+               PatternSize*2 +
+               (PatternMask ? 1+PatternSize*2+1+PatternSize*2+1 : 0) +
+               AsciiStrLen(" by ") +
+               PatternSize*2 +
+               (ReplaceMask ? 1+PatternSize*2+1+PatternSize*2+1 : 0) +
+               AsciiStrLen(" at ofs:%X\n") + 8 // +16 for %X
+          ) * 1 + 1; // *2 for CHAR16, +1 for \0
+      CHAR8* buf = AllocateZeroPool(length);
+      AsciiSPrint(buf, length, "%aReplace ", buf );
       for (UINTN Index = 0; Index < PatternSize; ++Index) {
-        DEBUG((DEBUG_VERBOSE, "%02X", Pattern[Index]));
+        AsciiSPrint(buf, length, "%a%02X", buf, Pattern[Index]);
       }
       if ( PatternMask ) {
-        DEBUG((DEBUG_VERBOSE, "/" ));
+        AsciiSPrint(buf, length, "%a/", buf );
         for (UINTN Index = 0; Index < PatternSize; ++Index) {
-          DEBUG((DEBUG_VERBOSE, "%02X", PatternMask[Index]));
+          AsciiSPrint(buf, length, "%a%02X", buf, PatternMask[Index]);
         }
-        DEBUG((DEBUG_VERBOSE, "(" ));
+        AsciiSPrint(buf, length, "%a(", buf );
         for (UINTN Index = 0; Index < PatternSize; ++Index) {
-          DEBUG((DEBUG_VERBOSE, "%02X", Data[DataOff + Index]));
+          AsciiSPrint(buf, length, "%a%02X", buf, Data[DataOff + Index]);
         }
-        DEBUG((DEBUG_VERBOSE, ")" ));
+        AsciiSPrint(buf, length, "%a)", buf );
       }
-      DEBUG((DEBUG_VERBOSE, " by " ));
+      AsciiSPrint(buf, length, "%a by ", buf );
 
       if (ReplaceMask == NULL) {
         CopyMem (&Data[DataOff], (void*)Replace, PatternSize);
@@ -171,21 +184,23 @@ ApplyPatch (
       }
 
       for (UINTN Index = 0; Index < PatternSize; ++Index) {
-        DEBUG((DEBUG_VERBOSE, "%02X", Replace[Index]));
+        AsciiSPrint(buf, length, "%a%02X", buf, Replace[Index]);
       }
       if ( ReplaceMask ) {
-        DEBUG((DEBUG_VERBOSE, "/" ));
+        AsciiSPrint(buf, length, "%a/", buf );
         for (UINTN Index = 0; Index < PatternSize; ++Index) {
-          DEBUG((DEBUG_VERBOSE, "%02X", ReplaceMask[Index]));
+          AsciiSPrint(buf, length, "%a%02X", buf, ReplaceMask[Index]);
         }
-        DEBUG((DEBUG_VERBOSE, "(" ));
+        AsciiSPrint(buf, length, "%a(", buf );
         for (UINTN Index = 0; Index < PatternSize; ++Index) {
-          DEBUG((DEBUG_VERBOSE, "%02X", Data[DataOff + Index]));
+          AsciiSPrint(buf, length, "%a%02X", buf, Data[DataOff + Index]);
         }
-        DEBUG((DEBUG_VERBOSE, ")" ));
+        AsciiSPrint(buf, length, "%a)", buf );
       }
 
-      DEBUG((DEBUG_VERBOSE, " at ofs:%X\n", DataOff));
+      AsciiSPrint(buf, length, "%a at ofs:%X\n", buf, DataOff);
+      DEBUG((DEBUG_VERBOSE, "%a", buf ));
+      FreePool(buf);
 
       ++ReplaceCount;
       DataOff += PatternSize;
