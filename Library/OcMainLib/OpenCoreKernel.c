ab6742000960471c32d17016d22ab24127c754cf
jief666 <github.com@jfa.knudsen.ovh>
2021-09-23 12:17:34 +0300

    Prevent log loop.

--- Library/OcMainLib/OpenCoreKernel.c
+++ Library/OcMainLib/OpenCoreKernel.c
@@ -1131,6 +1131,10 @@ OcKernelFileOpen (
 
   Status = OcSafeFileOpen (This, NewHandle, FileName, OpenMode, Attributes);
 
+#ifdef CLOVER_BUILD
+  extern int g_OpeningLogFile;
+  if ( !g_OpeningLogFile ) {
+#endif
   DEBUG ((
     DEBUG_VERBOSE,
     "OC: Opening file %s with %u mode gave - %r\n",
@@ -1138,6 +1142,9 @@ OcKernelFileOpen (
     (UINT32) OpenMode,
     Status
     ));
+#ifdef CLOVER_BUILD
+  }
+#endif
 
   //
   // Hook kernelcache read attempts for fuzzy kernelcache matching.
