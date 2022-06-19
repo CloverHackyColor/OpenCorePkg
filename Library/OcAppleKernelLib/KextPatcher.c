8aa646b4470e9480abd16a3aadf79cfc814073d4
jief666 <github.com@jfa.knudsen.ovh>
2022-06-19 15:19:20 +0200

    Handle full name for symbol in PatcherGetSymbolAddress.

--- Library/OcAppleKernelLib/KextPatcher.c
+++ Library/OcAppleKernelLib/KextPatcher.c
@@ -222,6 +222,9 @@ PatcherGetSymbolAddress (
   UINT64          SymbolAddress;
   UINT32          Offset;
   UINT32          Index;
+  BOOLEAN        FullName = FALSE;
+  
+  FullName = (Name[0] == '_' && Name[1] == '_');
 
   Index  = 0;
   Offset = 0;
@@ -256,7 +259,10 @@ PatcherGetSymbolAddress (
     }
 
     SymbolName = MachoGetSymbolName (&Context->MachContext, Symbol);
-    if ((SymbolName != NULL) && (AsciiStrCmp (Name, SymbolName) == 0)) {
+//    if ((SymbolName != NULL) && (AsciiStrCmp (Name, SymbolName) == 0)) {
+    if (SymbolName != NULL &&
+        (( FullName && AsciiStrCmp (Name, SymbolName) == 0) ||
+         (!FullName && AsciiStrStr (SymbolName, Name) != NULL))) {
       //
       // Once we have a symbol, get its ondisk offset.
       //
